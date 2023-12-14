721f74e70003e540339c22934755526217b32799
Ben Ratelade <ratelade.benjamin@gmail.com>
2023-12-15 09:08:29 +1100

    Slightly better file generation

--- spec/lib/day_11/solver_spec.rb
+++ spec/lib/day_11/solver_spec.rb
@@ -1,8 +1,35 @@
 # frozen_string_literal: true
-require "day_11/solver'"
+
+require "day_11/solver"
 require "pry"
 
 RSpec.describe Solver do
   before do
+    File.create("test-file.txt", "w") do |file|
+      file << <<~CONTENT
+        ...#......
+        .......#..
+        #.........
+        ..........
+        ......#...
+        .#........
+        .........#
+        ..........
+        .......#..
+        #...#.....
+      CONTENT
+    end
+  end
+
+  after do
+    File.delete("test-file.txt")
+  end
+
+  describe "#solve_part_1" do
+    it "returns the sum of distances between all galaxies" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.solve_part_1).to eq(374)
+    end
   end
 end
--- spec/lib/day_16/solver_spec.rb
+++ spec/lib/day_16/solver_spec.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
-require "day_16/solver'"
+require "day_16/solver"
 require "pry"
 
 RSpec.describe Solver do
   before do
+    File.create("test-file.txt", "w") do |file|
+      file << <<~FILE_CONTENT
+      FILE_CONTENT
+    end
+  end
+
+  after do
+    File.delete("test-file.txt")
   end
 end
