a49c8fd4b7682a749bc63a60c6573d7ca9e75891
Ben Ratelade <ratelade.benjamin@gmail.com>
2023-12-28 22:07:28 +0100

    day 25 start

---
+++ spec/lib/day_25/solver_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+require "day_25/solver"
+require "pry"
+
+RSpec.describe Solver do
+  before do
+    File.open("test-file.txt", "w") do |file|
+      file << <<~FILE_CONTENT
+        jqt: rhn xhk nvd
+        rsh: frs pzl lsr
+        xhk: hfx
+        cmg: qnr nvd lhk bvb
+        rhn: xhk bvb hfx
+        bvb: xhk hfx
+        pzl: lsr hfx nvd
+        qnr: nvd
+        ntq: jqt hfx bvb xhk
+        nvd: lhk
+        lsr: lhk
+        rzs: qnr cmg lsr rsh
+        frs: qnr lhk lsr
+      FILE_CONTENT
+    end
+  end
+
+  after do
+    File.delete("test-file.txt")
+  end
+end
